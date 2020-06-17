4fc654566576767137ff9d8717a93e381a648440
jetsundev <exelspice@gmail.com>
2020-06-17 13:01:13 +0300

    add services and other

---
+++ components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="nav">
+    <nuxt-link to="/" class="nav-link">Real world Events</nuxt-link>
+    <nav>
+      <nuxt-link to="/" class="nav-link">List</nuxt-link>
+      <nuxt-link to="/event/create" class="nav-link">Create</nuxt-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 70px;
+  background: rgb(220, 248, 214);
+  &-link {
+    padding: 0 20px;
+    text-decoration: none;
+    font-size: 20px;
+    font-weight: 500;
+  }
+}
+</style>
---
+++ pages/event/create.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Create event</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Here you can create an event'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss"></style>
