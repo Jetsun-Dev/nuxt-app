5feeb916ca8ce34cbde9da01afe60ca3c1241500
jetsundev <exelspice@gmail.com>
2020-07-04 09:39:58 +0300

    add about page

---
+++ pages/about/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>
+        About page
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'About page'
+    }
+  }
+}
+</script>
+
+<style></style>
